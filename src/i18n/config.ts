64979673f8b281e68a993394c9d4637eabf237a6
Pedro Durek <pedro_hmd@hotmail.com>
2020-11-14 23:10:39 -0600

    Setup config for i18n

---
+++ src/i18n/config.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import ns1 from './en/ns1.json';
+import ns2 from './en/ns2.json';
+// import ns3 from "./en/ns3.json";
+
+export const resources = {
+  en: {
+    ns1,
+    ns2,
+    // ns3,
+  },
+} as const;
+
+i18n.use(initReactI18next).init({
+  resources,
+});
