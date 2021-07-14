b4d40f6bc08aae131f2e05e52736c15a59122641
Pedro Durek <pedro.durek@shopify.com>
2021-07-13 22:58:58 -0500

    Update react-i18next and i18next

--- @types/react-i18next/index.d.ts
+++ @types/react-i18next/index.d.ts
@@ -2,6 +2,7 @@ import 'react-i18next';
 import { resources } from 'i18n/config';
 
 declare module 'react-i18next' {
-  type DefaultResources = typeof resources['en'];
-  interface Resources extends DefaultResources {}
+  interface CustomTypeOptions {
+    resources: typeof resources['en'];
+  }
 }
--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,13 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import ns1 from './en/ns1.json';
 import ns2 from './en/ns2.json';
-// import ns3 from "./en/ns3.json";
+import ns3 from './en/ns3.json';
 
 export const resources = {
   en: {
     ns1,
     ns2,
-    // ns3,
+    ns3,
   },
 } as const;
 
